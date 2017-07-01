6cd064a31b629ce79b1ff2549dbb10ddb1fc27cd
cw2310 <cw2310@naver.com>
2017-07-01 22:00:33 +0900

    Baekjoon
    단계별 문제 Chapter1

---
+++ Chapter1/3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Sub {
+	public static void main(String args[]){
+		int A, B;
+		Scanner s = new Scanner(System.in);
+		A = s.nextInt();
+		B = s.nextInt();
+		s.close();
+		
+		System.out.print(A-B);
+	}
+}
